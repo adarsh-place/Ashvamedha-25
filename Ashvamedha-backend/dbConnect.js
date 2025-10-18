9fd79f2c155a02796c2cfbd4b1c5d90e7b7a3ec3
Adarsh Chandra <adarshworkplace@gmail.com>
2025-10-18 15:31:25 +0530

    DB change

--- Ashvamedha-backend/dbConnect.js
+++ Ashvamedha-backend/dbConnect.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 module.exports = async () => {
   
   // const mongoURL = `mongodb+srv://meenakshi:${process.env.DB_ACCESS_KEY}@cluster0.4g5xdvl.mongodb.net/?retryWrites=true&w=majority`;
-  const mongoURL = `mongodb+srv://nishanttomar910:${process.env.DB_ACCESS_KEY}@cluster0.feh0c.mongodb.net/?retryWrites=true&w=majority`
+  // const mongoURL = `mongodb+srv://nishanttomar910:${process.env.DB_ACCESS_KEY}@cluster0.feh0c.mongodb.net/?retryWrites=true&w=majority`
+  const mongoURL = `mongodb+srv://ashvamedha_25:${process.env.DB_ACCESS_KEY}@cluster0.lg9pgj8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   try {
     const connect = await mongoose.connect(mongoURL);
     console.log(`mongodb connected ${connect.connection.host}`);
