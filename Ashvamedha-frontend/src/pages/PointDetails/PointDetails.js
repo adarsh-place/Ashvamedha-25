008fbd42ebf996e8c441064891d1bd5107b5310a
Adarsh Chandra <adarshworkplace@gmail.com>
2025-10-18 16:50:13 +0530

    Bug Fix

--- Ashvamedha-frontend/src/pages/PointDetails/PointDetails.js
+++ Ashvamedha-frontend/src/pages/PointDetails/PointDetails.js
@@ -5,16 +5,21 @@ import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer/Footer.js";
 import Navbar from "../../components/Navbar/Navbar.js";
 import PointInfo from "../../components/PointInfo/PointInfo.js";
+import Loader from "../../components/Loader/Loader.js";
 import axios from "axios";
 import { setLoading } from "../../redux/appSlice.js";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import {server} from "../../constants.js"
+import { server } from "../../constants.js";
 import { useSelector } from "react-redux";
 
 const head = {
-  sportName: 'SPORT', category: 'CAT.', matchName: 'MATCH NAME', college1: 'OPP.', point: 'PTS.' 
-}
+  sportName: "SPORT",
+  category: "CAT.",
+  matchName: "MATCH NAME",
+  college1: "OPP.",
+  point: "PTS.",
+};
 
 function PointDetails() {
   const dispatch = useDispatch();
@@ -24,14 +29,10 @@ function PointDetails() {
   async function fetchdata() {
     try {
       dispatch(setLoading(true));
-      const response = await axios.post(
-        `${server}/college/`,
-        {
-          collegeName: params.collegename,
-        }
-      );
+      const response = await axios.post(`${server}/college/`, {
+        collegeName: params.collegename,
+      });
       // console.log(response.data.result.modifiedResult);
-      
 
       setPointinfo(response.data.result.modifiedResult);
     } catch (e) {
@@ -43,37 +44,39 @@ function PointDetails() {
   useEffect(() => {
     fetchdata();
   }, []);
-  return pointinfo.length === 0 ? (
+
+  return (
     <div className="pd">
       <Navbar />
-      {/* <div class="wrapper">
-        <h3 class="typing">
-          Your team is yet to score <br />
-        </h3>
-      </div> */}
-      <div className="wrapper">
-        <h2 >Your team is yet to score!!</h2>
-      </div>
-      <Footer />
-    </div>
-  ) : (
-    <div className="pd">
-      <Navbar />
-      <div className="main">
-        <h2 className="heading-point">
-          <span className="h1">POINTS</span>
-          <span className="h2">TABLE</span>
-        </h2>
-        {!isLoading && (
-          <div className="pointdetail">
-            <PointInfo gameInfo={head}/>
-            {pointinfo.map((item, index) => (
-              <PointInfo gameInfo={item} key={index} serialNo={index} />
-            ))}
-          </div>
-        )}
-      </div>
-      {/* <Footer /> */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {pointinfo.length === 0 ? (
+            <>
+              <div className="wrapper">
+                <h2>Your team is yet to score!!</h2>
+              </div>
+              <Footer />
+            </>
+          ) : (
+            <div className="main">
+              <h2 className="heading-point">
+                <span className="h1">POINTS</span>
+                <span className="h2">TABLE</span>
+              </h2>
+              {!isLoading && (
+                <div className="pointdetail">
+                  <PointInfo gameInfo={head} />
+                  {pointinfo.map((item, index) => (
+                    <PointInfo gameInfo={item} key={index} serialNo={index} />
+                  ))}
+                </div>
+              )}
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
