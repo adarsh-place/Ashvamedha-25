76ccf22266edfbd3b5782ee8d2d6c15f75c40637
Adarsh Chandra <adarshworkplace@gmail.com>
2025-11-08 03:30:13 +0530

    Fixtures update

--- Ashvamedha-frontend/src/components/FixtureCard/FixtureCard2.js
+++ Ashvamedha-frontend/src/components/FixtureCard/FixtureCard2.js
@@ -5,8 +5,8 @@ function FixtureCard2(props) {
   return (
     <div className="score-card">
         <div className="part2">
-            <div className="set-details setname">{props.match.matchType.toUpperCase()}</div>      
-            <div className="set-details">{ props.match.category.toUpperCase()}</div>
+            <div className="set-details setname">{props.match.matchType}</div>      
+            <div className="set-details">{ props.match.category}</div>
             <div className="set-details">{props.match.time}</div>
             <div className="set-details location">
               Location : {props.match.location}
